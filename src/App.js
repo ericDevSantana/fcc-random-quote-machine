637a5acc55593be630718ccb50b81799343f533a
Eric <22939634+ericDevSantana@users.noreply.github.com>
2021-05-31 16:06:23 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import "./App.css";
 import Card from "../src/components/Card";
 import Quote from "../src/components/Quote";
+
+
+// https://api.quotable.io/random  fetch random quote using this link https://github.com/lukePeavey/quotable credits
 
 function App() {
   return (
