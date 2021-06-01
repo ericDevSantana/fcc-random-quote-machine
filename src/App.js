7ed28bf452baf919b69e5bc1d132fb5f79192461
Eric <22939634+ericDevSantana@users.noreply.github.com>
2021-06-01 03:17:59 -0700

    comments

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Quote from "../src/components/Quote";
 // https://api.quotable.io/random  fetch random quote using this link https://github.com/lukePeavey/quotable credits
 
 function App() {
+  const [newReq, setNewReq] = useState(false); //everytime newReq changes its state a new request is triggered by changing useEffect dependencies
   const [quote, setQuote] = useState({
     quote: "",
     author: "",
   });
-  const [newReq, setNewReq] = useState(false);
 
+  // useEffect will fetch from this API and populate a new quote object...will trigger again when newReq changes its state
   useEffect(() => {
     fetch("https://api.quotable.io/random")
       .then((response) => response.json())
@@ -22,6 +23,7 @@ function App() {
       );
   }, [newReq]);
 
+  //passing callback down through Quote to be called when the button is clicked
   const quoteHandler = () => {
     setNewReq((prevState) => {
       return !prevState;
